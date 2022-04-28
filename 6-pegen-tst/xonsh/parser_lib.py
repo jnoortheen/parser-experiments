b46d642b1f02eb8b47da8a51b303695dc56de873
Noortheen Raja <jnoortheen@gmail.com>
2022-04-28 11:57:08 +0530

    feat: base off xonsh grammar from python.gram

---
+++ 6-pegen-tst/xonsh/parser_lib.py
@@ -0,0 +1,60 @@
+import ast
+import io
+import os
+import tokenize
+from typing import (
+    Any,
+    Callable,
+    Iterator,
+    Literal,
+    Union,
+    Optional,
+)
+
+from pegen.tokenizer import Tokenizer
+from .parser import XonshParser as Parser
+
+
+def parse_file(
+    path: str,
+    py_version: Optional[tuple] = None,
+    token_stream_factory: Optional[
+        Callable[[Callable[[], str]], Iterator[tokenize.TokenInfo]]
+    ] = None,
+    verbose: bool = False,
+) -> ast.Module:
+    """Parse a file."""
+    with open(path) as f:
+        tok_stream = (
+            token_stream_factory(f.readline)
+            if token_stream_factory
+            else tokenize.generate_tokens(f.readline)
+        )
+        tokenizer = Tokenizer(tok_stream, verbose=verbose, path=path)
+        parser = Parser(
+            tokenizer,
+            verbose=verbose,
+            filename=os.path.basename(path),
+            py_version=py_version,
+        )
+        return parser.parse("file")
+
+
+def parse_string(
+    source: str,
+    mode: Union[Literal["eval"], Literal["exec"]],
+    py_version: Optional[tuple] = None,
+    token_stream_factory: Optional[
+        Callable[[Callable[[], str]], Iterator[tokenize.TokenInfo]]
+    ] = None,
+    verbose: bool = False,
+) -> Any:
+    """Parse a string."""
+    tok_stream = (
+        token_stream_factory(io.StringIO(source).readline)
+        if token_stream_factory
+        else tokenize.generate_tokens(io.StringIO(source).readline)
+    )
+    tokenizer = Tokenizer(tok_stream, verbose=verbose)
+    parser = Parser(tokenizer, verbose=verbose, py_version=py_version)
+    return parser.parse(mode if mode == "eval" else "file")
